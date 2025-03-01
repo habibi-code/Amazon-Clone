016f3453ea95a3e752739918f8049c001641eb7b
Your Name <your@email.com>
2025-03-01 09:37:38 -0800

    header component

--- amazon_clone/src/Components/Header/Header.jsx
+++ amazon_clone/src/Components/Header/Header.jsx
@@ -1,61 +1,93 @@
-import React from 'react'
-import './Header.css'
-import { CiLocationOn } from "react-icons/ci";
+import React from 'react';
+import classes from './Header.module.css'; 
+import logoImg from '../../assets/logo.png';
+import { IoLocationOutline } from "react-icons/io5";
 import { FaSearch } from "react-icons/fa";
-import logoImg from "../../assets/logo.png"
-import { IoCartOutline } from "react-icons/io5";
+import { BiCart } from "react-icons/bi";
+
+
 const Header = () => {
   return (
-    <section>
-        <section>
-            <div className='logo'>
-                <a href="/">
-                <img src={logoImg} alt="amazon img" />
-                </a>
-                <span>
-                   <CiLocationOn />
-                </span>
-                <div>
-                    <p>Delivered to</p>
-                    <span>Ethiopia</span>
-                </div>
-            </div>
-            {/* Search bar */}
-            <div>
-                <select name="" id="">
-                    <option value="">All</option>
-                </select>
-                <input type="text" placeholder='Search Amazon'/>
-                <FaSearch />
-            </div>
-            {/* Right side */}
-            <div>
-            <div>
-                <img src="https://cdn.britannica.com/33/4833-050-F6E415FE/Flag-United-States-of-America.jpg" alt="flag" />
-                <select name="" id="">
-                    <option value="">EN</option>
-                </select>
-            </div>
-            {/* three components */}
-            <a href="/">
-            <div>
-                <p>Sign in</p>
-                <span>Acount & Lists</span>
-            </div>
-            </a>
-            <a href="/">
-            <p>returns</p>
-            <span>& Orders</span>
-            </a>
-            <a href="/">
-            <IoCartOutline />
-            <span>0</span>
-            </a>
+    <header className={classes.Header}>
+      {/* Logo */}
+      <div className={classes.header__logo}>
+        <img
+          src={logoImg}
+          alt="Amazon Logo"
+          className={classes.header__logoImage}
+        />
+      </div>
 
-            </div>
-        </section>
-    </section>
-  )
-}
+      {/* Location and Delivery Country */}
+      <div className={classes.header__location}>
+        <IoLocationOutline className={classes.header__locationIcon} />
+        <div className={classes.locationTxt}>
+        <span className={classes.header__locationText}>Deliver to</span>
+        <span className={classes.header__locationCountry}>United States</span>
+        </div>
+      </div>
 
-export default Header
+      {/* Search Bar */}
+      <div className={classes.header__search}>
+        {/* All Departments Dropdown using <select> */}
+      <div className={classes.header__allDepartments}>
+        <select className={classes.header__select}>
+          <option value="">All</option>
+          <option value="all">All Departments</option>
+          <option value="electronics">Electronics</option>
+          <option value="fashion">Fashion</option>
+          <option value="home">Home & Kitchen</option>
+          <option value="books">Books</option>
+          <option value="toys">Toys & Games</option>
+          <option value="sports">Sports & Outdoors</option>
+          {/* Add more departments as needed */}
+        </select>
+      </div>
+        <input
+          type="text"
+          className={classes.header__searchInput}
+          placeholder="Search Amazon"
+        />
+        <div className={classes.header__bg}>
+        <FaSearch className={classes.header__searchIcon} />
+        </div>
+      </div>
+
+      {/* Language Option using <select> */}
+      <div className={classes.header__language}>
+      <img src="https://cdn.countryflags.com/thumbs/united-states-of-america/flag-400.png" alt="flag" className={classes.header__languageIcon}/>
+        <select className={classes.language__select}>
+          <option value="en">EN</option>
+          <option value="es">ES</option>
+          <option value="fr">FR</option>
+          <option value="de">DE</option>
+          {/* Add more languages as needed */}
+        </select>
+      </div>
+
+      {/* Navigation */}
+      <div className={classes.header__nav}>
+        <div className={classes.header__option}>
+          <span className={classes.header__optionLineOne}>Hello, Sign In</span>
+          <span className={classes.header__optionLineTwo}>Account & Lists</span>
+        </div>
+        <div className={classes.header__option}>
+          <span className={classes.header__optionLineOne}>Returns</span>
+          <span className={classes.header__optionLineTwo}>& Orders</span>
+        </div>
+        <div className={classes.header__option}>
+          <span className={classes.header__optionLineOne}>Your</span>
+          <span className={classes.header__optionLineTwo}>Prime</span>
+        </div>
+      </div>
+
+      {/* Cart Icon */}
+      <div className={classes.header__cart}>
+        <BiCart className={classes.header__cartIcon} />
+        <span className={classes.header__cartCount}>0</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
