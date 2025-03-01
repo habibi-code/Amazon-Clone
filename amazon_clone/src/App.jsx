649dd76f1f123812a7d5d1228f42bda4524c8339
Your Name <your@email.com>
2025-03-01 04:32:55 -0800

    header structure

--- amazon_clone/src/App.jsx
+++ amazon_clone/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import './App.css'
+import Header from './Components/Header/Header'
 
 const App = () => {
   return (
     <div>
-  
+  <Header />
     </div>
   )
 }
--- amazon_clone/src/Components/Header/Header.jsx
+++ amazon_clone/src/Components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Header.css'
+import { CiLocationOn } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa";
 import logoImg from "../../assets/logo.png"
+import { IoCartOutline } from "react-icons/io5";
 const Header = () => {
   return (
     <section>
@@ -10,7 +13,7 @@ const Header = () => {
                 <img src={logoImg} alt="amazon img" />
                 </a>
                 <span>
-                    {/* icon */}
+                   <CiLocationOn />
                 </span>
                 <div>
                     <p>Delivered to</p>
@@ -22,11 +25,33 @@ const Header = () => {
                 <select name="" id="">
                     <option value="">All</option>
                 </select>
-                {/* icon */}
+                <input type="text" placeholder='Search Amazon'/>
+                <FaSearch />
             </div>
             {/* Right side */}
             <div>
-                
+            <div>
+                <img src="https://cdn.britannica.com/33/4833-050-F6E415FE/Flag-United-States-of-America.jpg" alt="flag" />
+                <select name="" id="">
+                    <option value="">EN</option>
+                </select>
+            </div>
+            {/* three components */}
+            <a href="/">
+            <div>
+                <p>Sign in</p>
+                <span>Acount & Lists</span>
+            </div>
+            </a>
+            <a href="/">
+            <p>returns</p>
+            <span>& Orders</span>
+            </a>
+            <a href="/">
+            <IoCartOutline />
+            <span>0</span>
+            </a>
+
             </div>
         </section>
     </section>
